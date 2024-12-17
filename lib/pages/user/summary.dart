e323f14be78ca6d09d5bf4dd1553217666856114
NevrCode <kevin12keval@gmail.com>
2024-12-17 15:54:04 +0700

    sad

--- lib/pages/user/summary.dart
+++ lib/pages/user/summary.dart
@@ -1,3 +1,4 @@
+import 'package:built_your_pc/util/util.dart';
 import 'package:flutter/material.dart';
 
 class SummaryPage extends StatefulWidget {
@@ -10,6 +11,14 @@ class SummaryPage extends StatefulWidget {
 class _SummaryPageState extends State<SummaryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: CostumText(
+          data: "Summary",
+          size: 14,
+        ),
+      ),
+    );
   }
 }
